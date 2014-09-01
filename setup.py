11dc740d24f02907127eee82e4ca6f99b46c4a62
ARASHI, Jumpei <jumpei.arashi@arashike.com>
2014-09-01 18:24:34 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='blackbird-elb',
+    version='0.1.0',
+    description=(
+        'Get stats of aws Elastic Load Blancing by using cloudwatch API.'
+    ),
+    author='ARASHI, Jumpei',
+    author_email='jumpei.arashi@arashike.com',
+    url='https://github.com/Vagrants/blackbird-elb',
+    install_requires=[
+        'boto'
+    ],
+    classifiers=[
+        'Programing Language :: Python :: 2.6',
+        'Programing Language :: Python :: 2.7',
+    ]
+)
