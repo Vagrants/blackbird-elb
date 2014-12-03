a5eb5bf0ce4db4f232ddd392e64939bbad4bb16a
ARASHI, Jumpei <jumpei.arashi@arashike.com>
2014-12-04 01:54:00 +0900

    Fix TYPO Programing -> Programming

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
         'boto'
     ],
     classifiers=[
-        'Programing Language :: Python :: 2.6',
-        'Programing Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
     ]
 )
